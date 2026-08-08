6ed4be5cfb13d11f36339a9d17ad8ca5611b5e87
Tingfe <tyongs@163.com>
2026-08-08 12:58:57 +0800

    fix(car): prevent status bar from reclaiming top space

--- src/components/common/StatusBar.tsx
+++ src/components/common/StatusBar.tsx
@@ -1,9 +1,11 @@
 import { useTheme } from '@/store/theme/hook'
 import { StatusBar as RNStatusBar } from 'react-native'
+import { isCarEdition } from '@/utils/nativeModules/utils'
 
 const StatusBar = function() {
   const theme = useTheme()
   const statusBarStyle = theme.isDark ? 'light-content' : 'dark-content'
+  if (isCarEdition) return null
   return <RNStatusBar backgroundColor="rgba(0,0,0,0)" barStyle={statusBarStyle} translucent={true} />
 }
 
